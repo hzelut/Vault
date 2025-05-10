a9e16d8f50f0e5056212c3a806015c69221ead77
hzelut <hzelut@gmail.com>
2025-05-11 01:19:25 +0900

    logout

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,13 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, make_response, redirect, url_for
 
 vault = Blueprint('vault', __name__)
 
 @vault.route('/')
 def index():
   return render_template('index.html')
+
+@vault.route('/logout')
+def logout():
+  res = make_response(redirect(url_for('blog.index')))
+  res.set_cookie('token', '', expires=0)
+  return res
