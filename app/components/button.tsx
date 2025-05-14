dc0d87c3abc043b2d8ddb34dd6acec63a3b0e8d8
hzelut <hzelut@gmail.com>
2025-05-15 01:48:31 +0900

    Button - memo icon

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -30,10 +30,17 @@ const clockIcon = () => (
     <path d="M16.24 16.24L12 12"/>
   </svg>
 )
+const memoIcon = () => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M20 14V7C20 5.34315 18.6569 4 17 4H7C5.34315 4 4 5.34315 4 7V17C4 18.6569 5.34315 20 7 20H13.5M20 14L13.5 20M20 14H15.5C14.3954 14 13.5 14.8954 13.5 16V20"/>
+    <path d="M8 8H16"/>
+    <path d="M8 12H12"/>
+  </svg>
+)
 
 type ButtonProps = {
-  type: 'calendar' | 'save' | 'clock'
-  onClick: React.MouseEventHandler<HTMLButtonElement>
+  type: 'calendar' | 'save' | 'clock' | 'memo'
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
 
@@ -50,11 +57,23 @@ export default function Button({ type, onClick }: ButtonProps) {
     case 'clock':
       icon = clockIcon
       break
+    case 'memo':
+      icon = memoIcon
+      break
   }
 
-  return (
-    <button onClick={onClick} className={styles.button}>
-      {icon()}
-    </button>
-  )
+  if(onClick) {
+    return (
+      <button onClick={onClick} className={styles.button}>
+        {icon()}
+      </button>
+    )
+  }
+  else {
+    return (
+      <div className={styles.button}>
+        {icon()}
+      </div>
+    )
+  }
 }
