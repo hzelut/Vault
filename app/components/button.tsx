83aa929d6438f90b43603033ee0ade57f40b30a8
hzelut <hzelut@gmail.com>
2025-05-15 07:42:43 +0900

    Button - close icon

--- app/components/button.tsx
+++ app/components/button.tsx
@@ -42,6 +42,12 @@ const repeatIcon = () => (
     <path d="M2.078,17.562c-0.01,-0.039 -0.016,-0.08 -0.016,-0.123c0,-0.138 0.056,-0.263 0.147,-0.353c0.008,-0.009 1.416,-1.417 2,-2c0.198,-0.198 0.507,-0.183 0.707,-0c0.199,0.183 0.185,0.522 -0,0.707l-1.147,1.146l15.669,0c0.828,0 1.5,-0.671 1.5,-1.5l0,-3.439c0,-0.276 0.224,-0.5 0.5,-0.5c0.276,0 0.5,0.224 0.5,0.5l0,3.439c0,1.381 -1.12,2.5 -2.5,2.5l-15.669,0l1.147,1.147c0.198,0.198 0.183,0.507 -0,0.707c-0.183,0.199 -0.522,0.185 -0.707,-0l-2,-2c-0.066,-0.063 -0.11,-0.143 -0.131,-0.231Zm19.845,-11.105c0.01,0.039 0.015,0.08 0.015,0.122c0,0.138 -0.056,0.263 -0.147,0.354c-0.008,0.008 -1.416,1.417 -2,2c-0.197,0.198 -0.507,0.183 -0.707,-0c-0.199,-0.183 -0.185,-0.522 0,-0.707l1.147,-1.147l-15.669,0c-0.828,0 -1.5,0.672 -1.5,1.5l0,3.439c-0,0.276 -0.224,0.5 -0.5,0.5c-0.276,0 -0.5,-0.224 -0.5,-0.5l0,-3.439c0,-1.381 1.12,-2.5 2.5,-2.5l15.669,0l-1.146,-1.146c-0.198,-0.198 -0.183,-0.507 -0,-0.707c0.183,-0.199 0.522,-0.185 0.707,-0l2,2c0.065,0.063 0.11,0.143 0.131,0.231Z"/>
   </svg>
 )
+const closeIcon = () => (
+  <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path d="M3 21.32L21 3.32001"/>
+    <path d="M3 3.32001L21 21.32"/>
+  </svg>
+)
 
 const iconMap = {
   calendar: calendarIcon,
@@ -49,18 +55,20 @@ const iconMap = {
   clock: clockIcon,
   memo: memoIcon,
   repeat: repeatIcon,
+  close: closeIcon,
 }
 
 type ButtonProps = {
   type: keyof typeof iconMap
   onClick?: React.MouseEventHandler<HTMLDivElement>
+  className?: string
 }
 
-export default function Button({ type, onClick }: ButtonProps) {
+export default function Button({ type, onClick, className}: ButtonProps) {
   const icon = iconMap[type]
 
   return (
-    <div onClick={onClick} className={styles.button}>
+    <div onClick={onClick} className={`${styles.button} ${className}`}>
       {icon()}
     </div>
   )
